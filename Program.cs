72f3ccf6ce823800d348b3fb5cfde6f42bc01feb
cookie35 <hakyeong35@gmail.com>
2025-02-05 17:57:35 +0900

    수정

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TextRPG
 {
@@ -10,38 +11,196 @@ namespace TextRPG
             Console.WriteLine("원하시는 이름을 설정해주세요.");
             string name = Console.ReadLine();
             Console.WriteLine("");
-            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
-            Console.WriteLine("");
-            Console.WriteLine("1. 상태 보기");
-            Console.WriteLine("2. 인벤토리");
-            Console.WriteLine("3. 상점");
-            Console.WriteLine("");
-            Console.WriteLine("원하는 행동을 입력하세요.");
-            int action = int.Parse(Console.ReadLine());
 
-            if(action == 0 || action > 3)
+            Intro();
+
+
+
+            //처음 나올 페이지
+            void Intro()
+            {
+                Console.WriteLine("");
+                Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
+                Console.WriteLine("");
+                Console.WriteLine("1. 상태 보기");
+                Console.WriteLine("2. 인벤토리");
+                Console.WriteLine("3. 상점");
+                Console.WriteLine("");
+                Console.WriteLine("원하는 행동을 입력하세요.");
+                int action = int.Parse(Console.ReadLine());
+                
+                Repeat(action);
+                
+
+            }
+
+
+            void Repeat(int num)
+            {
+                Console.Clear();
+                if (num == 0 || num > 3)
+                {
+                    Console.WriteLine("잘못된 입력입니다.");
+                    Console.WriteLine("");
+                    Intro();
+                }
+
+                else if (num == 1)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("상태 보기");
+                    Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+                    Console.WriteLine("");
+                    Console.WriteLine("Lv. 01");
+                    Console.WriteLine("Chad ( 전사 )");
+                    Console.WriteLine("공격력 : 10");
+                    Console.WriteLine("방어력: 5");
+                    Console.WriteLine("체 력 : 100");
+                    Console.WriteLine("Gold: 1500 G");
+                    Console.WriteLine("");
+                    Console.WriteLine("0. 나가기");
+                    Console.WriteLine("");
+                    Console.WriteLine("원하시는 행동을 입력해주세요.");
+                    int repeatNum = int.Parse(Console.ReadLine());
+                    Menu(repeatNum);
+
+
+                }
+
+                else if (num == 2)
+                {
+                    inventory();
+
+                }
+
+            }
+         
+            
+            void Menu(int Menunum)
+            {
+                if (Menunum == 0)
+                {
+                    Intro();
+                }
+
+                else
+                {
+                    ReEnter();
+                }
+
+            }
+
+            void ReEnter()
+            {
+
+                Console.WriteLine("잘못된 입력입니다.");
+                Console.WriteLine("");
+                int againNum = int.Parse(Console.ReadLine());
+                Menu(againNum);
+
+            }
+
+
+            void inventory()
+            {
+                Console.WriteLine("");
+                Console.WriteLine("인벤토리");
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+                Console.WriteLine("");
+                Console.WriteLine("[아이템 목록]");
+                Console.WriteLine("1.장착 관리");
+                Console.WriteLine("0. 나가기");
+                Console.WriteLine("");
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                int invenNum = int.Parse(Console.ReadLine());
+                inven(invenNum);
+            }
+
+
+            void inven(int invenNum)
+            {
+                if (invenNum == 0)
+                {
+                    Intro();
+                }
+
+                else if (invenNum == 1)
+                {
+                    Manage();
+                }
+
+                else
+                {
+                    reEnter2();
+                }
+
+            }
+
+            
+            void reEnter2()
             {
                 Console.WriteLine("잘못된 입력입니다.");
-                Console.WriteLine("다시 원하는 행동을 입력하세요.");
-                action = int.Parse(Console.ReadLine());
-
+                Console.WriteLine("");
+                int repetNum2 = int.Parse(Console.ReadLine());
+                inven(repetNum2);
             }
 
-            else
+            // 장착 관리 페이지
+            void Manage()
             {
-                Console.WriteLine("예.");
+                Console.WriteLine("인벤토리 - 장착 관리");
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+                Console.WriteLine("");
+                Console.WriteLine("[아이템 목록]");
+                Console.WriteLine("");
+                Console.WriteLine("- 1 무쇠갑옷      | 방어력 +5 | 무쇠로 만들어져 튼튼한 갑옷입니다.");
+                Console.WriteLine("- 2 스파르타의 창  | 공격력 +7 | 스파르타의 전사들이 사용했다는 전설의 창입니다.");
+                Console.WriteLine("- 3 낡은 검         | 공격력 +2 | 쉽게 볼 수 있는 낡은 검 입니다.");
+                Console.WriteLine("");
+                Console.WriteLine("0. 나가기");
+                Console.WriteLine("");
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                int manageNum = int.Parse(Console.ReadLine());
+                Manage2(manageNum);
+
             }
-            ////rtrtrtrtrtrtt
+
+
+            void Manage2(int manageNum)
+            {
+                if (manageNum == 0)
+                {
+                    inventory();
+                    
+                }
+
+                else if (manageNum == 1)
+                {
+
+                }
+
+                else if (manageNum == 2)
+                {
+
+                }
+
+                else if (manageNum == 3)
+                {
+
+                }
+
+                else
+                {
+
+                }
+
+
+            }
+
+
+
+
         }
 
     }
 }
-
-// 스파르타 마을에 오신 여러분 환영합니다.
-// 이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.
-
-// 1. 상태 보기
-// 2. 인벤토리
-// 3. 상점
-
-// 원하시는 행동을 입력해주세요.>>
--- obj/Debug/net8.0/TextRPG.AssemblyInfo.cs
+++ obj/Debug/net8.0/TextRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6610b189fb3c38ede98304c8acb91ee7d4da37c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc08a3dbfbcc7f9e2055e6da9f6f1f7e72c12cae")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
